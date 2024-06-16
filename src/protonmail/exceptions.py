44b69d9eb56ece7f4ea2b0ba2dd24ae322ce673f
opulentfox-29 <3acqw2bx@duck.com>
2024-06-16 21:28:33 +0300

    automation get all keys

--- src/protonmail/exceptions.py
+++ src/protonmail/exceptions.py
@@ -7,3 +7,15 @@ class SendMessageError(Exception):
 
 class InvalidTwoFactorCode(Exception):
     """Invalid Two-Factor Authentication(2FA) code"""
+
+
+class NoKeysForDecryptThisMessage(Exception):
+    """
+    No keys for decrypt this message,
+    maybe you created new key then re-login,
+    if you deleted key then you can't decrypt this message
+    """
+
+
+class LoadSessionError(Exception):
+    """Error while load session, maybe this session file was created in other version? then re-login"""
